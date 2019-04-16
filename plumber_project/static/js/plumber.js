4702e5e29d8153cc32f779f9c34ff3792dd401dd
balazsmolnr <balazsmolnr@gmail.com>
2019-04-16 13:25:49 +0200

    drag and drop functions

---
+++ plumber_project/static/js/plumber.js
@@ -0,0 +1,12 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function dragStart(ev) {
+    id = ev.target.id;
+}
+
+function drop(ev) {
+    ev.target.append(document.getElementById(id));
+
+}
