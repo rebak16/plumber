ebabde084b3b82587e10ac0f4f87576e679f1ac3
Kruppa Tamás <krpp.tamas@gmail.com>
2019-04-17 11:50:43 +0200

    dragable picture is unrotetable + you cant drop the picture in wrong place

--- plumber_project/static/js/plumber.js
+++ plumber_project/static/js/plumber.js
@@ -14,7 +14,10 @@ function dragStart(ev) {
 }
 
 function drop(ev) {
-    ev.target.append(document.getElementById(id));
+    if (ev.target.name !== "picture"){
+        ev.target.append(document.getElementById(id));
+    }
+
 
 }
 
@@ -22,46 +25,55 @@ function addImages () {
     let newBox = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${0}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/1.png";
+    img.name = "picture";
     newBox.appendChild(img);
 
     let newBox1 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${0}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/2.png";
+    img.name = "picture";
     newBox1.appendChild(img);
 
     let newBox2 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${1}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/9.png";
+    img.name = "picture";
     newBox2.appendChild(img);
 
     let newBox3 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/5.png";
+    img.name = "picture";
     newBox3.appendChild(img);
 
     let newBox4 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/2_c.png";
+    img.name = "picture";
     newBox4.appendChild(img);
 
     let newBox5 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${3}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/2_b.png";
+    img.name = "picture";
     newBox5.appendChild(img);
 
     let newBox6 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${3}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/2_a.png";
+    img.name = "picture";
     newBox6.appendChild(img);
 
     let newBox7 = document.querySelector(`.game-cell[data-coordinate-x="${2}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/leak.png";
+    img.name = "picture";
     newBox7.appendChild(img);
 
     let newBox8 = document.querySelector(`.game-cell[data-coordinate-x="${3}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
     img.src = "static/images/white/8.png";
+    img.name = "picture";
     newBox8.appendChild(img);
 }
 addImages();
@@ -71,7 +83,7 @@ function rotate() {
     let cells = document.querySelectorAll('.game-cell');
     for (let cell of cells) {
         cell.addEventListener('click',(event) => {
-            if (event.target.className !== 'game-cell') {
+            if (event.target.className !== 'game-cell' && event.target.className !== 'drag') {
                 img = event.target;
                 angle = (angle+90)%360;
                 img.className = "image_rotate_"+angle;
