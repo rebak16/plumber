178ac995b57837ceba620dbe9ed254058b1ab715
Kruppa Tamás <krpp.tamas@gmail.com>
2019-04-17 11:08:10 +0200

    white and blue plumbs

--- plumber_project/static/js/plumber.js
+++ plumber_project/static/js/plumber.js
@@ -1,6 +1,6 @@
 var angle = 0;
 var counter =0;
-var originalcells = document.querySelectorAll('.game-cell')
+var originalcells = document.querySelectorAll('.game-cell');
 for (let originalcell of originalcells) {
     originalcell.textContent = "";
 }
@@ -21,47 +21,47 @@ function drop(ev) {
 function addImages () {
     let newBox = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${0}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/1.png";
+    img.src = "static/images/white/1.png";
     newBox.appendChild(img);
 
     let newBox1 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${0}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forRotate/2.png";
+    img.src = "static/images/white/2.png";
     newBox1.appendChild(img);
 
     let newBox2 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${1}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/9.png";
+    img.src = "static/images/white/9.png";
     newBox2.appendChild(img);
 
     let newBox3 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/5.png";
+    img.src = "static/images/white/5.png";
     newBox3.appendChild(img);
 
     let newBox4 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/2_c.png";
+    img.src = "static/images/white/2_c.png";
     newBox4.appendChild(img);
 
     let newBox5 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${3}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/2_b.png";
+    img.src = "static/images/white/2_b.png";
     newBox5.appendChild(img);
 
     let newBox6 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${3}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/2_a.png";
+    img.src = "static/images/white/2_a.png";
     newBox6.appendChild(img);
 
     let newBox7 = document.querySelector(`.game-cell[data-coordinate-x="${2}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/leak.png";
+    img.src = "static/images/white/leak.png";
     newBox7.appendChild(img);
 
     let newBox8 = document.querySelector(`.game-cell[data-coordinate-x="${3}"].game-cell[data-coordinate-y="${2}"]`);
     var img = document.createElement("img");
-    img.src = "static/images/forDrag/8.png";
+    img.src = "static/images/white/8.png";
     newBox8.appendChild(img);
 }
 addImages();
