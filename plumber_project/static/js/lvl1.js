77f79922d31cf4eba26c84d67482ad42e686c874
Kruppa Tamás <krpp.tamas@gmail.com>
2019-04-17 13:37:59 +0200

    lvl1.js + whiteWithGrey forder

---
+++ plumber_project/static/js/lvl1.js
@@ -0,0 +1,61 @@
+function lvl1 () {
+    let newBox = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${0}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/whiteWithGrey/1_b.png";
+    img.name = "picture";
+    img.className = "drag";
+    newBox.appendChild(img);
+
+    let newBox1 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${0}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/white/2_b.png";
+    img.name = "picture";
+    newBox1.appendChild(img);
+
+    let newBox2 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${2}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/whiteWithGrey/5.png";
+    img.className = "drag";
+    img.name = "picture";
+    newBox2.appendChild(img);
+
+    let newBox3 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${2}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/white/2_b.png";
+    img.name = "picture";
+    newBox3.appendChild(img);
+
+    let newBox4 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${3}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/white/2_c.png";
+    img.name = "picture";
+    newBox4.appendChild(img);
+
+    let newBox5 = document.querySelector(`.game-cell[data-coordinate-x="${3}"].game-cell[data-coordinate-y="${2}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/whiteWithGrey/7.png";
+    img.name = "picture";
+    img.className = "drag";
+    newBox5.appendChild(img);
+
+    let newBox6 = document.querySelector(`.game-cell[data-coordinate-x="${4}"].game-cell[data-coordinate-y="${2}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/white/2_c.png";
+    img.name = "picture";
+    newBox6.appendChild(img);
+
+    let newBox7 = document.querySelector(`.game-cell[data-coordinate-x="${4}"].game-cell[data-coordinate-y="${3}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/white/2_c.png";
+    img.name = "picture";
+    newBox7.appendChild(img);
+
+    let newBox8 = document.querySelector(`.game-cell[data-coordinate-x="${4}"].game-cell[data-coordinate-y="${4}"]`);
+    var img = document.createElement("img");
+    img.src = "static/images/whiteWithGrey/8_b.png";
+    img.name = "picture";
+    img.className = "drag";
+    newBox8.appendChild(img);
+}
+lvl1();
+
--- plumber_project/static/js/plumber.js
+++ plumber_project/static/js/plumber.js
@@ -11,73 +11,14 @@ function allowDrop(ev) {
 
 function dragStart(ev) {
     id = ev.target.id;
+    console.log(id)
 }
 
 function drop(ev) {
     if (ev.target.name !== "picture"){
         ev.target.append(document.getElementById(id));
     }
-
-
 }
-
-function addImages () {
-    let newBox = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${0}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/1.png";
-    img.name = "picture";
-    newBox.appendChild(img);
-
-    let newBox1 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${0}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/2.png";
-    img.name = "picture";
-    newBox1.appendChild(img);
-
-    let newBox2 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${1}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/9.png";
-    img.name = "picture";
-    newBox2.appendChild(img);
-
-    let newBox3 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${2}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/5.png";
-    img.name = "picture";
-    newBox3.appendChild(img);
-
-    let newBox4 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${2}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/2_c.png";
-    img.name = "picture";
-    newBox4.appendChild(img);
-
-    let newBox5 = document.querySelector(`.game-cell[data-coordinate-x="${0}"].game-cell[data-coordinate-y="${3}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/2_b.png";
-    img.name = "picture";
-    newBox5.appendChild(img);
-
-    let newBox6 = document.querySelector(`.game-cell[data-coordinate-x="${1}"].game-cell[data-coordinate-y="${3}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/2_a.png";
-    img.name = "picture";
-    newBox6.appendChild(img);
-
-    let newBox7 = document.querySelector(`.game-cell[data-coordinate-x="${2}"].game-cell[data-coordinate-y="${2}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/leak.png";
-    img.name = "picture";
-    newBox7.appendChild(img);
-
-    let newBox8 = document.querySelector(`.game-cell[data-coordinate-x="${3}"].game-cell[data-coordinate-y="${2}"]`);
-    var img = document.createElement("img");
-    img.src = "static/images/white/8.png";
-    img.name = "picture";
-    newBox8.appendChild(img);
-}
-addImages();
-
 
 function rotate() {
     let cells = document.querySelectorAll('.game-cell');
@@ -93,15 +34,14 @@ function rotate() {
 }
 rotate();
 
-
 function winCondition() {
-    let cells = document.querySelectorAll('.game-cell');
+/*    let cells = document.querySelectorAll('.game-cell');
     for (let cell of cells) {
         cell.addEventListener('click',(event) => {
-            counter = (counter+1)%3;
+            counter = (counter+1)%4;
             event.target.textContent = counter;
             console.log(event.target.className);
         });
-    }
+    }*/
 }
 winCondition();
